d811578da1841093e0bcbc64d68e81fcb14fd058
Rajput Neha Jitendra <100520546+Rajput-Neha-Jitendra@users.noreply.github.com>
2022-04-24 16:21:45 +0530

    Object Oriented programming:CPP
    Here is 1 program which is base on oop concept.....Class and Object

---
+++ 1oop.cpp
@@ -0,0 +1,52 @@
+
+// C++ program to demonstrate 
+// accessing of data members 
+ 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class oop
+{
+
+    // Access specifier
+
+    public:
+ 
+
+    // Data Members
+
+    string cpp;
+ 
+
+    // Member Functions()
+
+    void printname()
+
+    {
+
+       cout << "Object Oriented programming is : " << cpp;
+
+    }
+};
+ 
+
+int main() {
+ 
+
+    // Declare an object of class geeks
+
+    oop obj1;
+ 
+
+    // accessing data member
+
+    obj1.cpp = " C plus plus";
+ 
+
+    // accessing member function
+
+    obj1.printname();
+
+    return 0;
+}
